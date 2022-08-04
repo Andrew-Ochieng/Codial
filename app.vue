a7c0854b819d8f0977598485db6ae882cd266197
AndrewMartial <andrewomosh44@gmail.com>
2022-08-04 18:40:20 +0300

    add clients section

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import { font-awesome } from "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
+// import { font-awesome } from "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
 
 export default {
   
