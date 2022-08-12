679d404397318853100e671ae38c1b75a52a114f
Code-X <andrewomosh44@gmail.com>
2022-08-12 17:28:05 +0300

    netlify deploy test

---
+++ components/workProcess.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="how-we-work" class="bg-sky-300 ">
+        <div class="md:px-12 px-8 md:py-24 py-8 text-center md:flex justify-around md:space-x-8 ">
+            <div class="my-4 border-2 border-white rounded-xl p-4">
+                <div>
+                    <img src="../assets/images/" alt="">
+                </div>
+                <div>
+                    <h3>Interactive Design</h3>
+                    <p> We follow a unique approach to create outstanding customer experience for your business.</p>
+                </div>
+            </div>
+            <div class="my-4 border-2 border-white rounded-xl p-4">
+                <div>
+                    <img src="../assets/images/" alt="">
+                </div>
+                <div>
+                    <h3>Interactive Design</h3>
+                    <p>Greater scope of scalability help attract & manage new opportunities arising for your business in the future.</p>
+                </div>
+            </div>
+            <div class="my-4 border-2 border-white rounded-xl p-4">
+                <div>
+                    <img src="../assets/images/" alt="">
+                </div>
+                <div>
+                    <h3>Interactive Design</h3>
+                    <p>Create extraordinary value for business with an app which is tailored specifically for your business needs</p>
+                </div>
+            </div>
+            <div class="my-4 border-2 border-white rounded-xl p-4">
+                <div>
+                    <img src="../assets/images/" alt="">
+                </div>
+                <div>
+                <h3>Interactive Design</h3>
+                <p>Consistent structure, flawless coding, innovative UI/UX, and seamless experience are the basic aspects of our  solutions.</p></div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style  scoped>
+
+</style>
